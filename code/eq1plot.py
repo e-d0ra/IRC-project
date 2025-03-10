ac58bc81e17902f960c66dfecebef5b58ff115e5
wuheiming02 <hilary.wu21@imperial.ac.uk>
2025-03-10 17:01:59 +0000

    Update eq1plot.py

--- code/eq1plot.py
+++ code/eq1plot.py
@@ -14,10 +14,12 @@ flux_values = calculate_flux(r_values)
 
 # Plotting the graph
 plt.plot(r_values, flux_values)
-plt.xlabel('Radius (r)')  # Label for x-axis
-plt.ylabel('Flux (F)')    # Label for y-axis
-plt.title('Flux Equation')  # subject to change
+plt.xlabel('Distance between point source and detector (m)')  # Label for x-axis
+plt.ylabel('Expected count rate (Bq)')    # Label for y-axis
+plt.title('Equation 1')  # subject to change
 plt.grid(True)  # Adding grid for better visualization
+plt.xlabel(0, 0.1)
+plt.ylabel(0, 80000)
 plt.savefig('../code/diagrams/eq1.png', dpi=400)
 plt.show()
 
