67ef3a47a6b525ab3ffa644b19eace2921a3aca9
wuheiming02 <hilary.wu21@imperial.ac.uk>
2025-03-10 17:21:22 +0000

    Update eq1plot.py

--- code/eq1plot.py
+++ code/eq1plot.py
@@ -9,7 +9,7 @@ def calculate_flux(r):
     return L / (4 * np.pi * r**2)
 
 # Generate values for r (radius)
-r_values = np.linspace(1, 15, 1000)  # Example: radius values from 1 to 100
+r_values = np.linspace(0.01, 15, 1000)  # Example: radius values from 1 to 100
 flux_values = calculate_flux(r_values)
 
 # Plotting the graph
@@ -19,7 +19,7 @@ plt.ylabel('Expected count rate (Bq)')    # Label for y-axis
 plt.title('Equation 1')  # subject to change
 plt.grid(True)  # Adding grid for better visualization
 plt.xlim(0, 15)
-plt.ylim(0, 80000)
+plt.ylim(0, 500000)
 plt.savefig('../code/diagrams/eq1.png', dpi=400)
 plt.show()
 
